73ed397379e598e078720f6392d6ea85d502f7e8
collin <tjzhangcong@outlook.com>
2021-10-25 20:23:21 +0800

    add cors support

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,7 +1,9 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "./mongo";
 import verify from "./jwt";
+import allowCors from "./cors";
 
-const wrapper = (handler: NextApiHandler) => dbConnect(verify(handler));
+const wrapper = (handler: NextApiHandler) =>
+  dbConnect(verify(allowCors(handler)));
 
 export default wrapper;
