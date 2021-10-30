6015d1a417e6183055f4eb8d19afbe7e0e4594cd
collin <tjzhangcong@outlook.com>
2021-10-30 16:17:00 +0800

    test cors

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -3,7 +3,8 @@ import dbConnect from "./mongo";
 import verify from "./jwt";
 import allowCors from "./cors";
 
-const wrapper = (handler: NextApiHandler) =>
-  dbConnect(allowCors(verify(handler)));
-
+const wrapper = (handler: NextApiHandler) => {
+  console.log("before cors");
+  return dbConnect(allowCors(verify(handler)));
+};
 export default wrapper;
--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -26,6 +26,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log("before cors");
   // Run cors
   await cors(req, res);
 
