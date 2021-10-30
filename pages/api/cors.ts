b5bd5f0f34a291ccdcaa778258d6f77950e83841
collin <tjzhangcong@outlook.com>
2021-10-30 16:10:06 +0800

    try cors

---
+++ pages/api/cors.ts
@@ -0,0 +1,34 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+import Cors from "cors";
+
+const initMiddleware = (middleware: any) => {
+  return (req: NextApiRequest, res: NextApiResponse) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+};
+
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run cors
+  await cors(req, res);
+
+  // Rest of the API logic
+  res.json({ message: "Hello Everyone!" });
+}
