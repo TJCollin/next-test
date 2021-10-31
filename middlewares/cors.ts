e6b9b7a707674f9158f43de93678158cb302e70a
collin <tjzhangcong@outlook.com>
2021-10-31 17:12:14 +0800

    test

--- middlewares/cors.ts
+++ middlewares/cors.ts
@@ -10,9 +10,8 @@ const allowCors =
       req.headers.origin?.includes("localhost")
     );
 
-    if (req.headers.origin?.includes("localhost")) {
-      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-    }
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET,OPTIONS,PATCH,DELETE,POST,PUT"
@@ -21,6 +20,7 @@ const allowCors =
       "Access-Control-Allow-Headers",
       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     );
+
     if (req.method === "OPTIONS") {
       res.status(200).end();
       return;
